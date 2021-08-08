dd9cdcd693ba59c3eacf3a6e12988befe0746de2
Corey <corey.w.calhoun@gmail.com>
2021-08-08 01:34:29 -0500

    fixed issue with db and useeffect on _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,34 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import PostFeed from '../components/PostFeed';
 import Sidebar from '../components/Sidebar';
+import Login from '../components/Login'
 import '../styles/globals.css';
+import { useSelector } from 'react-redux'
+import { selectUser } from '../utils/userSlice';
+
 function App() {
+
+  // const user = useSelector(selectUser);
+
   return (
     <div className="flex-col min-h-screen bg-indigo-100">
       <Header />
-      {/* main body */}
+
+      {/*if user not logged in, render Login. otherwise render the main content */}
+      {/* {!user ? (
+        <Login />
+      ) : ( */}
+      {/* //render main body */}
       <main className="flex md:w-3/4 md:mx-auto ">
         <Sidebar />
         <PostFeed />
       </main>
+      {/* )} */}
+
+
       <Footer />
     </div>
+
 
 
   )
