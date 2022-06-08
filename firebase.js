2140b79a7dcd15ccd152a3de528ab7938a3c3814
Corey C <67290010+corey-calhoun@users.noreply.github.com>
2022-06-08 20:36:50 +0000

    updated firebase creds

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBR8UQ8TMFHioy23fN3NTXzn-dIpyQpunU",
-    authDomain: "social-media-demo-36b5f.firebaseapp.com",
-    projectId: "social-media-demo-36b5f",
-    storageBucket: "social-media-demo-36b5f.appspot.com",
-    messagingSenderId: "263460314123",
-    appId: "1:263460314123:web:16156227aad0f0ed033b0e",
-    measurementId: "G-2H8HGJKN48"
+    apiKey: "AIzaSyAgvTB76mK3l6cGjkgxn8DDo6PsVFsvxj8",
+    authDomain: "social-network-v1-d7307.firebaseapp.com",
+    projectId: "social-network-v1-d7307",
+    storageBucket: "social-network-v1-d7307.appspot.com",
+    messagingSenderId: "1005972028417",
+    appId: "1:1005972028417:web:a0262648a5231f073c9400",
+    measurementId: "G-FF6LKZ7GN5"
 };
 
 //this is what connects us to the db and sets everytt
